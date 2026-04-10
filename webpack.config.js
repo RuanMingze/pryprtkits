8b6cb2850cc39766b5f26a5a2085948bfe7304be
Ruan Mingze <xmt20160124@outlook.com>
2026-04-10 19:43:38 +0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,96 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'pryprtkits.js',
-    library: {
-      name: 'PryprtKits',
-      type: 'umd',
-      export: 'default',
-    },
-    globalObject: 'this',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                targets: {
-                  browsers: ['> 1%', 'last 2 versions', 'not dead'],
-                },
-              }],
-            ],
-          },
-        },
+module.exports = [
+  {
+    name: 'unminified',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'pryprtkits.js',
+      library: {
+        name: 'PryprtKits',
+        type: 'umd',
+        export: 'default',
       },
-    ],
-  },
-  plugins: [],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            drop_debugger: true,
-          },
-          format: {
-            comments: false,
+      globalObject: 'this',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  targets: {
+                    browsers: ['> 1%', 'last 2 versions', 'not dead'],
+                  },
+                }],
+              ],
+            },
           },
         },
-        extractComments: false,
-      }),
-    ],
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
   },
-  resolve: {
-    extensions: ['.js'],
+  {
+    name: 'minified',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'pryprtkits.min.js',
+      library: {
+        name: 'PryprtKits',
+        type: 'umd',
+        export: 'default',
+      },
+      globalObject: 'this',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  targets: {
+                    browsers: ['> 1%', 'last 2 versions', 'not dead'],
+                  },
+                }],
+              ],
+            },
+          },
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+              drop_debugger: true,
+            },
+            format: {
+              comments: false,
+            },
+          },
+          extractComments: false,
+        }),
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
   },
-};
+];
